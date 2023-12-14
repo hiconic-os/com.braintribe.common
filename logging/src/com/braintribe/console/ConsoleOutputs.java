52424097d3ed7b1355296d58f4f408ebf57aed62
Peter Gazdik <peter.gazdik@gmail.com>
2023-12-14 21:24:44 +0100

    deferred migration after established CICD pipeline

--- logging/src/com/braintribe/console/ConsoleOutputs.java
+++ logging/src/com/braintribe/console/ConsoleOutputs.java
@@ -1,16 +1,7 @@
-// ============================================================================
-// Copyright BRAINTRIBE TECHNOLOGY GMBH, Austria, 2002-2022
-// 
-// This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
-// 
-// This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public License along with this library; See http://www.gnu.org/licenses/.
-// ============================================================================
 package com.braintribe.console;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -55,6 +46,13 @@ public interface ConsoleOutputs extends ConsoleStyles {
 
 	static ConsoleText text(Consumer<Appendable> writer) {
 		return new ConsoleDynamicText(writer);
+	}
+
+	static ConsoleText stackTrace(Throwable t) {
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		t.printStackTrace(pw);
+		return text(sw.toString());
 	}
 
 	static ConfigurableConsoleOutputContainer configurableSequence() {
